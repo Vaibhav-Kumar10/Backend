ee170d5d865e1d727a9ecd5be74c7d2ce29cc789
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2025-02-18 21:40:49 +0530

    Update run.py

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ app = create_app()
 app.register_blueprint(routes)  # Register the routes blueprint
 
 CORS(app)  # This enables CORS globally for the entire app
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')  # Run the app in debug mode
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0')  # Run the app in debug mode
